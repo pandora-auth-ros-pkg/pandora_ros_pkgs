0293163a4948348fe0071ff5c6ef3dc6200dc81b
paschalidoud <paschalidoud@gmail.com>
2014-03-14 19:44:35 +0200

    Merge remote-tracking branch 'origin/hydro-devel' into hydro-devel

--- pandora_vision/pandora_vision_motion/include/pandora_vision_motion/motion_detection.h
+++ pandora_vision/pandora_vision_motion/include/pandora_vision_motion/motion_detection.h
@@ -3,7 +3,7 @@
 * Software License Agreement (BSD License)
 *
 *  Copyright (c) 2014, P.A.N.D.O.R.A. Team.
-*  All rights reserved.
+*  All rights reserved.cd
 *
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
@@ -35,7 +35,7 @@
 * Author:  George Aprilis
 *********************************************************************/
  
- #ifndef MOTIONDETECTION_H
+#ifndef MOTIONDETECTION_H
 #define  MOTIONDETECTION_H
 
 #include "ros/ros.h"
@@ -58,78 +58,118 @@
 #include <iostream>
 #include <stdlib.h>
 
-#define DEFAULT_HEIGHT			480		//default frame height
-#define DEFAULT_WIDTH			640		//default frame width
+//!< Default frame height
+#define DEFAULT_HEIGHT 480		
+//!< Default frame width
+#define DEFAULT_WIDTH	640		
 
+namespace pandora_vision
+{
+  class MotionDetection : public StateClient 
+  {
+    private:
 
-class MotionDetection : public StateClient {
-	private:
+      //nodeHandle
+      ros::NodeHandle _nh;
+      MotionDetector*	_motionDetector;
+      float ratioX;
+      float ratioY;
+      
+      float hfov;		//horizontal Field Of View (rad)
+      float vfov;		
+      int frameWidth;		//frame width
+      int frameHeight;	//frame height
+      
+      cv::Mat	motionFrame;				// frame processed by MotionDetector
+    
+      
+      ros::Time	motionFrameTimestamp;		// MotionDetector frame timestamp
 
-		//nodeHandle
-		ros::NodeHandle _nh;
-		MotionDetector*	_motionDetector;
-		float ratioX;
-		float ratioY;
-		
-		float hfov;		//horizontal Field Of View (rad)
-		float vfov;		
-		int frameWidth;		//frame width
-		int frameHeight;	//frame height
-		
-		cv::Mat		motionFrame;				// frame processed by MotionDetector
-		cv::Mat		extraFrame;					// copy frame processed by MotionDetector
-		
-		ros::Time		motionFrameTimestamp;		// MotionDetector frame timestamp
+      std::string imageTopic;
+      std::string cameraName;
+      std::string cameraFrameId;
+      
+      //publishers for MotionDetector result messages
+      ros::Publisher _motionPublisher;
+      
+      //the subscriber that listens to the frame topic advertised by the central node
+      image_transport::Subscriber _frameSubscriber;
+      
+      //debug publisher for MotionDetector
+      image_transport::Publisher _motionDiffPublisher;
+      image_transport::Publisher _motionFrmPublisher;
+      
+      // variables for changing in dummy msg mode for debugging
+      bool motionDummy;
+      // variables for changing in debug mode. Publish images for debugging
+      bool debugMotion;
+      
+      //variable used for State Managing
+      bool motionNowON;
+      
+        /**
+   @brief Publishing debug images
+   @return void
+      */
+      void publish_debug_images();
+    public:
+          
+      /**
+        @brief Constructor
+      **/
+      MotionDetection();
+            
+      /**
+        @brief Destructor
+      */			
+      virtual ~MotionDetection();	
+      
+       /**
+         @brief Get parameters referring to view and frame characteristics from
+         launch file
+         @return void
+      */
+      void getGeneralParams();
+      
+      /**
+        @brief Get parameters referring to motion detection algorithm
+        @return void
+      */
+      void getMotionParams();
+      
+      /**
+        @brief This method uses a MotionDetector instance to detect motion
+        in current frame.
+        @return void
+      */
+      void motionCallback();
 
-		string imageTopic;
-		string cameraName;
-		string cameraFrameId;
-		
-		//publishers for MotionDetector result messages
-		ros::Publisher _motionPublisher;
-		
-		//the subscriber that listens to the frame topic advertised by the central node
-		image_transport::Subscriber _frameSubscriber;
-		
-		//debug publisher for MotionDetector
-		image_transport::Publisher _motionDiffPublisher;
-		image_transport::Publisher _motionFrmPublisher;
-		
-		// variables for changing in dummy msg mode for debugging
-		bool motionDummy;
-		// variables for changing in debug mode. Publish images for debugging
-		bool debugMotion;
-		
-		//variable used for State Managing
-		bool motionNowON;
-	
-		void publish_debug_images();
-	public:
-				
-		//constructor
-		MotionDetection();
-					
-		//destructor			
-		~MotionDetection();	
-		
-		//get parameters from launch file
-		void getGeneralParams();
-		void getMotionParams();
-		
-		//executes motion Detector when in state STATE_ARM_SEARCH_COMPLETED
-		void motionDetectAndPost();
-
-		//get a new image
-		void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-		
-		//Implemented from StateClient
-		void startTransition(int newState);
-		void completeTransition(void);
-		
-		int curState;		//Current state of robot
-		int prevState;		//Previous state of robot
-};
-
+       /**
+         @brief Function called when new ROS message appears, for front camera
+         @param msg [const sensor_msgs::ImageConstPtr&] The message
+         @return void
+      */
+      void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+      
+      /**
+        @brief Node's state manager
+        @param newState [int] The robot's new state
+        @return void
+      */
+      void startTransition(int newState);
+      
+      /**
+         @brief After completion of state transition
+         @return void
+      */
+      void completeTransition(void);
+      
+      //!< Current state of robot
+      int curState;		
+      //!< Previous state of robot
+      int prevState;		
+  };
+}
 #endif
 		
 		
