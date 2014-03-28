812f07adbb3e3e5f841aa3be1794e5177d98e57d
Anna Minou <ann.minou@gmail.com>
2014-03-28 21:32:37 +0200

    update kinematic

--- pandora_control/scripts/kinematic.py
+++ pandora_control/scripts/kinematic.py
@@ -8,19 +8,16 @@ import serial
 from time import sleep
 from geometry_msgs.msg import Twist
 
-class Kinematic:
+sub = None
+serial_left_right = serial.Serial('/dev/ttyUSB0', 9600) 
+wheel_separation = 0.4392
+wheel_diameter = 0.0889
+rpm_max = 100
+
+def __init__(self):
+  sub = rospy.Subscriber("cmd_vel", Twist, self.twist_callback)
   
-  pub = None
-  serial_left = serial.Serial('/dev/ttyUSB0', 9600) 
-  serial_right = serial.Serial('/dev/ttyUSB1', 9600) 
-  wheel_separation = 0.4392
-  wheel_diameter = 0.0889
-  rpm_max = 100
-  
-  def __init__(self):
-    pub = rospy.Subscriber("cmd_vel", Twist, self.twist_callback)
-  
-  def twist_callback(self, msg):
+ def twist_callback(self, msg):
     
     wheel_speed_left = msg.linear.x - msg.angular.z * self.wheel_separation / 2.0
     wheel_speed_right = msg.linear.x + msg.angular.z * self.wheel_separation / 2.0
@@ -30,13 +27,14 @@ class Kinematic:
     
     rpm_left  = rps_left/(self.rpm_max*2*3.14/60) 
     rpm_right  = rps_right/(self.rpm_max*2*3.14/60)
-    
-    print int(rpm_left*255), int(rpm_right*255)
-    self.serial_left.write(str(int(rpm_left*255)))
-    self.serial_right.write(str(int(rpm_left*255)))
+  
+  
+  command = "$L" + '{:+03d}'.format(left_speed)*rpm_left + "R" + '{:+03d}'.format(right_speed)*rpm_right
+  self.serial_left_right.write(command)
+  
 
 
-if __name__ == '__main__':
-  rospy.init_node('kinematic')
-  joy = Kinematic()
-  rospy.spin()
+
+
+  
+  
