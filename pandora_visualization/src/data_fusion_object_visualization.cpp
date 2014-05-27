b5569e55f00247faa604ea4de9ab4740e89052ce
Tsirigotis Christos <chritsir@auth.gr>
2014-05-27 16:16:03 +0300

    Changes about LandoltC and DataMatrix to be imported. [ci skip]

--- pandora_visualization/src/data_fusion_object_visualization.cpp
+++ pandora_visualization/src/data_fusion_object_visualization.cpp
@@ -1,8 +1,8 @@
 #include "ros/ros.h"
 
 #include <tf/transform_broadcaster.h>
-#include "data_fusion_communications/GetObjectsSrv.h"
-#include "data_fusion_communications/GetMarkersSrv.h"
+#include "pandora_data_fusion_msgs/GetObjectsSrv.h"
+#include "pandora_data_fusion_msgs/GetMarkersSrv.h"
 
 typedef std::vector<geometry_msgs::PoseStamped> poseStampedVector;
 
@@ -14,6 +14,8 @@ class ObjectVisualization {
 	ros::Timer _broadcastTimer2;
 
 	ros::Publisher _hazmat_marker_pub ; 
+	ros::Publisher _landoltc_marker_pub ; 
+	ros::Publisher _dataMatrix_marker_pub ; 
 	ros::Publisher _hole_marker_pub ;
 	ros::Publisher _qr_marker_pub ;
 	ros::Publisher _thermal_marker_pub ;
@@ -21,17 +23,15 @@ class ObjectVisualization {
 	ros::Publisher _victims_to_go_marker_pub ;
 
 	// broadcasters fot tf
-	tf::TransformBroadcaster _holeFrameBroadcaster;
-	tf::TransformBroadcaster _qrFrameBroadcaster;
-	tf::TransformBroadcaster _hazmatFrameBroadcaster;
+	tf::TransformBroadcaster _frameBroadcaster;
 	
 	ros::ServiceClient _getObjectsClient;
 	ros::ServiceClient _getMarkersClient;
 
 	ros::NodeHandle _nh;
 
-	data_fusion_communications::GetObjectsSrv _objectsSrv;
-	data_fusion_communications::GetMarkersSrv _markersSrv;
+	pandora_data_fusion_msgs::GetObjectsSrv _objectsSrv;
+	pandora_data_fusion_msgs::GetMarkersSrv _markersSrv;
 	
 	void broadcastTimerCb(const ros::TimerEvent& event);
 	
@@ -66,13 +66,15 @@ ObjectVisualization::ObjectVisualization(){
 	//~ ros::Duration(15).sleep();
 	
 	
-	_getObjectsClient = _nh.serviceClient<data_fusion_communications::GetObjectsSrv>
+	_getObjectsClient = _nh.serviceClient<pandora_data_fusion_msgs::GetObjectsSrv>
 		("/data_fusion/get_objects");
 		
-	_getMarkersClient = _nh.serviceClient<data_fusion_communications::GetMarkersSrv>
+	_getMarkersClient = _nh.serviceClient<pandora_data_fusion_msgs::GetMarkersSrv>
 		("/data_fusion/get_markers");
 
 	_hazmat_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("hazmats_markers", 1);
+	_landoltc_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("landoltc_markers", 1);
+	_dataMatrix_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("dataMatrix_markers", 1);
 	_hole_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("holes_markers", 1);
 	_qr_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("qrs_markers", 1);
 	_thermal_marker_pub = _nh.advertise<visualization_msgs::MarkerArray>("thermals_markers", 1);
@@ -91,10 +93,11 @@ void ObjectVisualization::broadcastTimerCb(const ros::TimerEvent& event){
 	
 	broadcastPoseVector(_objectsSrv.response.holes);
 	broadcastPoseVector(_objectsSrv.response.qrs);
+	broadcastPoseVector(_objectsSrv.response.landoltcs);
+	broadcastPoseVector(_objectsSrv.response.dataMatrices);
 	broadcastPoseVector(_objectsSrv.response.hazmats);
 	broadcastPoseVector(_objectsSrv.response.thermals);
 	broadcastPoseVector(_objectsSrv.response.victimsToGo);
-	broadcastPoseVector(_objectsSrv.response.approachPoints);
 }
 
 void ObjectVisualization::broadcastTimerCb2(const ros::TimerEvent& event){
@@ -105,7 +108,7 @@ void ObjectVisualization::broadcastTimerCb2(const ros::TimerEvent& event){
 		ros::Duration(0.5).sleep();
 	}
 	
-	if (_markersSrv.response.holes.markers.size()>0 || _markersSrv.response.qrs.markers.size() || _markersSrv.response.hazmats.markers.size() >0){
+	if (_markersSrv.response.holes.markers.size()>0 || _markersSrv.response.qrs.markers.size()>0 || _markersSrv.response.hazmats.markers.size() >0){
 		
 		std::cout << "holes size: " << _markersSrv.response.holes.markers.size() << "\n";
 		std::cout << "qrs size: " << _markersSrv.response.qrs.markers.size() << "\n";
@@ -116,6 +119,9 @@ void ObjectVisualization::broadcastTimerCb2(const ros::TimerEvent& event){
 	_hazmat_marker_pub.publish(_markersSrv.response.qrs);
 	_qr_marker_pub.publish(_markersSrv.response.hazmats);
 	_thermal_marker_pub.publish(_markersSrv.response.thermals);
+	_landoltc_marker_pub.publish(_markersSrv.response.landoltcs);
+	_dataMatrix_marker_pub.publish(_markersSrv.response.dataMatrices);
+
 	_victims_visiter_marker_pub.publish(_markersSrv.response.victimsToGo);
 	_victims_to_go_marker_pub.publish(_markersSrv.response.victimsVisited);
 
@@ -133,7 +139,7 @@ void ObjectVisualization::broadcastPoseVector(poseStampedVector& vect)
 		ROS_DEBUG_NAMED("broadcastPoseVector","Publishing tf : %f , %f , %f , world to %s ",vec[0] ,vec[1] , vec[2] ,
 			it->header.frame_id.c_str() );
 		
-		_holeFrameBroadcaster.sendTransform( tf::StampedTransform( transVic, ros::Time::now(),
+		_frameBroadcaster.sendTransform( tf::StampedTransform( transVic, ros::Time::now(),
 			"world", it->header.frame_id ) );
 		
 	}
