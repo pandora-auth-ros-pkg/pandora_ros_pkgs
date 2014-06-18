b9ee9651e1c64d359e842fb2b51281c0d5945e5f
orestis <orestis@ee.auth.gr>
2014-06-18 18:55:10 +0300

    Change terminal settings to make usb work with ubuntu server

--- pandora_hardware_interface/pandora_arm_hardware_interface/arm_usb_interface/src/arm_usb_interface.cpp
+++ pandora_hardware_interface/pandora_arm_hardware_interface/arm_usb_interface/src/arm_usb_interface.cpp
@@ -62,6 +62,24 @@ ArmUSBInterface::ArmUSBInterface()
   /*Needs some time to initialize, even though it opens succesfully.
    tcflush() didn't work without waiting at least 8 ms*/
   ros::Duration(0.03).sleep();
+  
+  //To save time you can see and change terminal settings in command line with stty command,
+  //before implementing in software. Note: stty prefixes disabled flags with a dash.
+  //See:  http://man7.org/linux/man-pages/man3/termios.3.html
+  struct termios tios;
+
+  if (tcgetattr(fd, &tios) < 0) {
+    ROS_ERROR("init_serialport: Couldn't get term attributes\n");
+    return -1;
+  }
+
+  tios.c_lflag &= ~(ICANON | ISIG);
+
+  if( tcsetattr(fd, TCSANOW, &tios) < 0) {
+    ROS_ERROR("init_serialport: Couldn't set term attributes\n");
+    return -1;
+  }
+
 }
 
 ArmUSBInterface::~ArmUSBInterface()
